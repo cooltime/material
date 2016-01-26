cac3d0a0ef4a94b97a501c9f140f1b4b7980fec9
cooltime <fan.bai@changba.com>
2016-01-26 19:16:58 +0800

    test

--- front-end/timepicker/static/js/example.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $('#endTime').val(dateFormat(new Date()));
-    $('#endTime').datetimepicker({
-        format: "yyyy-mm-dd hh:ii:ss",
-        autoclose : true,
-    });
-})
